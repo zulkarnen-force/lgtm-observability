5c9efb09cce53a16fb3c3c98d39b62b37274f846
zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2026-07-10 22:37:14 +0700

    feat: change title to pt bit

--- apps/demo-nextjs/src/app/page.tsx
+++ apps/demo-nextjs/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="space-y-6">
       <div>
-        <h1 className="text-3xl font-bold">Demo App</h1>
+        <h1 className="text-3xl font-bold">PT Bentang Inspirasi Teknologi</h1>
         <p className="text-muted-foreground">
           The Art of Digital Government Transformation
         </p>
